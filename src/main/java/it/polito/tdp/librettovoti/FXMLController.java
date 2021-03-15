4fcef467998f96d4b38cc488044d58269599e15e
Lorenzo <Lorenzo@LORENZO.homenet.telecomitalia.it>
2021-03-15 16:31:22 +0100

    Libretto aggiornato fino al controllo sui doppioni da implementare

--- src/main/java/it/polito/tdp/librettovoti/FXMLController.java
+++ src/main/java/it/polito/tdp/librettovoti/FXMLController.java
@@ -1,25 +1,107 @@
 package it.polito.tdp.librettovoti;
 
 import java.net.URL;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.librettovoti.model.Libretto;
+import it.polito.tdp.librettovoti.model.Voto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
+public class FXMLController {
+
+	private Libretto model; 
+	
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtVoto;
+
+    //@FXML
+    //private TextField txtData;
     
     @FXML
-    private Label label;
-    
+    private DatePicker dataPicker;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextArea txtResult;
+
+    @FXML
+    void handleInserisci(ActionEvent event) {
+    	//Leggi e controlla
+    	String nomeE = txtNome.getText();
+    	if(nomeE.length() == 0) {
+    		txtResult.setText("ERRORE: nome esame vuoto");
+    		return;
+    	}
+    	
+    	String votoE = txtVoto.getText();
+    	int votoInt = 0;
+    	try{
+    		votoInt = Integer.parseInt(votoE);
+    	}catch(NumberFormatException ex){
+    		txtResult.setText("ERRORE: Formato voto incorretto");
+    		return;
+    	}
+    	if(votoInt < 18 || votoInt > 31) {
+    		txtResult.setText("ERRORE: Il voto deve essere compreso tra 18 e 31");
+    		return;
+    	}
+    	
+    	/*String datatE = txtData.getText();
+    	LocalDate data;
+    	try{
+    		data = LocalDate.parse(datatE);
+    	} catch(DateTimeParseException e) {
+    		txtResult.setText("ERRORE: Formato data incorretto");
+    		return;
+    	}*/
+    	
+    	LocalDate data = dataPicker.getValue();
+    		if(data == null) {
+    			txtResult.setText("ERRORE: Data mancante");
+        		return;
+    		}
+    	
+    	//TODO: verifica dati
+    	
+    	//Esegui
+    	Voto voto = new Voto(nomeE, votoInt,data);
+    	model.add(voto);
+    	
+    	//Aggiorna
+    	txtResult.setText(model.toString());
+    	txtNome.clear();
+    	txtVoto.clear();
+    	//txtData.clear();
+    	dataPicker.setValue(null) ;
+
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void setModel(Libretto model) {
+    	this.model = model;
+    }
+
+    @FXML
+    void initialize() {
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtVoto != null : "fx:id=\"txtVoto\" was not injected: check your FXML file 'Scene.fxml'.";
+        //assert txtData != null : "fx:id=\"txtData\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert dataPicker != null : "fx:id=\"dataPicker\" was not injected: check your FXML file 'Scene.fxml'.";
+        
+    }
 }
--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -1,18 +1,37 @@
 package it.polito.tdp.librettovoti.model;
 
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Libretto {
 	
 	private List<Voto> voti;
+	private Map<String, Voto> votiMap;
 
 	public Libretto() {
 		this.voti = new ArrayList<>();
+		this.votiMap = new HashMap<>();
 	}
 	
 	public void add(Voto v) {
 		this.voti.add(v);
+		this.votiMap.put(v.getCorso(), v);
+	}
+	
+	public Voto ricercaCorso(String Corso){
+		return this.votiMap.get(Corso);
+	}
+	
+	public List<Voto> listaVotiUguali(int p) {
+		List<Voto> ris = new ArrayList<>() ;
+		for(Voto v: this.voti) {
+			if(v.getVoto() == p) {
+				ris.add(v);
+			}
+		}
+		return ris;
 	}
 	
 	public Libretto votiUguali (int p) {
@@ -23,6 +42,50 @@ public class Libretto {
 			}
 		}
 		return ris;
+	}
+	
+	public boolean esisteDuplicato(Voto v) {
+//		boolean trovato = false;
+//		for(Voto voto: this.voti) {
+//			if(voto.getCorso().equals(v.getCorso()) && voto.getVoto() == v.getVoto()) {
+//				trovato = true;
+//				break;
+//			}
+//		}
+//		return trovato;
+		
+		Voto trovato = this.votiMap.get(v.getCorso());
+		if(trovato == null) {
+			return false;
+		}
+		if(trovato.getVoto() == v.getVoto()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean esisteConflitto(Voto v) {
+	//	boolean trovato = false;
+	//	for(Voto voto: this.voti) {
+	//		if(voto.getCorso().equals(v.getCorso()) && voto.getVoto() != v.getVoto()) {
+	//			trovato = true;
+	//			break;
+	//		}
+	//	}
+	//	return trovato;
+		
+		Voto trovato = this.votiMap.get(v.getCorso());
+		if(trovato == null) {
+			return false;
+		}
+		if(trovato.getVoto() == v.getVoto()) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	public String toString() {
